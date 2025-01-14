089870bd783e219f200a1cf8cbfe12d0fdcbf614
Aurora <zsz52ymy@gmail.com>
2025-01-14 20:36:41 +0800

    first

---
+++ run.py
@@ -0,0 +1,22 @@
+import json, os, tqdm, torch
+from diffusers import DiffusionPipeline
+
+max_num = 15
+dataset_root = "A"
+
+
+with torch.no_grad():
+    for taskid in tqdm.tqdm(range(0, max_num)):
+        pipe = DiffusionPipeline.from_pretrained(f"..\stabile_diffusion\stable-diffusion-2-1")
+        pipe.load_lora_weights(f"style_{taskid:02d}")
+        pipe = pipe.to("cuda")
+
+        # load json
+        with open(f"{dataset_root}/{taskid:02d}/prompt.json", "r") as file:
+            prompts = json.load(file)
+
+        for id, prompt in prompts.items():
+            print(prompt)
+            image = pipe(prompt + f" in style_{taskid:02d}", num_inference_steps=25).images[0]
+            os.makedirs(f"./output/{taskid:02d}", exist_ok=True)
+            image.save(f"./output/{taskid:02d}/{prompt}.png")
